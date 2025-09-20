c47f3097e226492ba95585f5c3ce72e7c616f2d3
Salah <salah_elhossiny856@yahoo.com>
2025-09-20 13:40:49 +0300

    added services

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,6 @@ import { ContactUs } from './pages/ContactUs';
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-white dark:bg-slate-900">
-      {/* <DarkModeToggle /> */}
-      {/* <Header /> */}
       <main>
         <Hero />
         <Services />
