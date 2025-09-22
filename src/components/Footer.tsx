ba55df6278b1bb7000e6270cce31bd60cd27f223
Salah <salah_elhossiny856@yahoo.com>
2025-09-22 06:27:20 +0300

    added services

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
 import GCMS_Logo from "../assets/GCMS_Logo.jpeg";
+import GCMS_Logo_without_bg from "../assets/GCMS_Logo_without_Background.png"; 
+
 
 export function Footer() {
   const footerLinks = {
@@ -29,7 +31,7 @@ export function Footer() {
           {/* Company Info */}
           <div className="space-y-4 lg:col-span-2">
             <img 
-              src={GCMS_Logo}
+              src={GCMS_Logo_without_bg}
               alt="GCMS Logo" 
               className="h-15 w-auto"
             />
@@ -75,7 +77,7 @@ export function Footer() {
                     <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                   </svg>
                 </a>
-                <a 
+                {/* <a 
                   href="https://www.upwork.com/agencies/1736655245525430272/" 
                   target="_blank" 
                   rel="noopener noreferrer"
@@ -85,7 +87,7 @@ export function Footer() {
                   <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                     <path d="M18.561 13.158c-1.102 0-2.135-.467-3.074-1.227l.228-1.076.008-.042c.207-1.143.849-3.06 2.839-3.06 1.492 0 2.703 1.212 2.703 2.703-.001 1.489-1.212 2.702-2.704 2.702zm0-8.14c-2.539 0-4.51 1.649-5.31 4.366-1.22-1.834-2.148-4.036-2.687-5.892H7.828v7.112c-.002 1.406-1.141 2.546-2.547 2.546-1.405 0-2.543-1.14-2.543-2.546V3.492H0v7.112c0 2.914 2.37 5.303 5.281 5.303 2.913 0 5.283-2.389 5.283-5.303v-1.19c.529 1.107 1.182 2.229 1.974 3.221l-1.673 7.873h2.797l1.213-5.71c1.063.679 2.285 1.109 3.686 1.109 3 0 5.439-2.452 5.439-5.45 0-3-2.439-5.439-5.439-5.439z"/>
                   </svg>
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
